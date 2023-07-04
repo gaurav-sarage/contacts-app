ac04543093348124b9c503bfde2d20a4aa0c4490
Gaurav <gauravsarage.1902@gmail.com>
2023-07-05 00:21:26 +0530

    Added contacts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SiteNav from './components/common/SiteNav';
 import HomePage from './components/home/HomePage';
 import LoginPage from './components/auth/LoginPage';
 import RegisterPage from "./components/auth/RegisterPage";
+import Contacts from './components/contacts/Contacts';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/' exact={true} element={<HomePage />} />
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
+          <Route path='/contacts' element={<Contacts />} />
         </Routes>
         <SiteFooter />        
       </header>
